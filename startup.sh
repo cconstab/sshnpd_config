7909dd6de511d289181f29eed1b63782855caaa1
Colin Constable <colin@constable.cc>
2024-02-22 14:49:22 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ssh-keygen -A
 /usr/sbin/sshd -D -o "ListenAddress 127.0.0.1" -o "PasswordAuthentication no"  &
-sudo -u $1 "/usr/bin/tmux new-session -d -s sshnpd && tmux send-keys -t sshnpd /home/ubuntu/.local/bin/sshnpd.sh C-m" 
-sudo -u $1 /bin/bash
+su - $1 sh -c "/usr/bin/tmux new-session -d -s sshnpd && tmux send-keys -t sshnpd /home/ubuntu/.local/bin/sshnpd.sh C-m" 
+su - $1
