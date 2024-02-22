d2ad868c25fe0df73d5dc70b6d96f9cac1a16491
Colin Constable <colin@constable.cc>
2024-02-22 15:24:34 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ ssh-keygen -A
 /usr/sbin/sshd -D -o "ListenAddress 127.0.0.1" -o "PasswordAuthentication no"  &
 su - $1 sh -c "/usr/bin/tmux new-session -d -s sshnpd && tmux send-keys -t sshnpd /home/ubuntu/.local/bin/sshnpd.sh C-m" 
 su - $1
-su - $1 sh -c "sleep infinity"
+su - $1 sh -c "/usr/bin/sleep infinity"
